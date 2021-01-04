787bbf4de15066cc2bef35334e11521b91d6d774
Mario Gonzalez <mario.gonzalez.a2599@gmail.com>
2021-01-04 05:06:56 -0600

    Resolviendo problemas de JS

--- src/main/webapp/assets/js/demo/datatables-demo.js
+++ src/main/webapp/assets/js/demo/datatables-demo.js
@@ -1,4 +1,16 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
-});
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('content-page');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    $('#dataTable').DataTable();
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
